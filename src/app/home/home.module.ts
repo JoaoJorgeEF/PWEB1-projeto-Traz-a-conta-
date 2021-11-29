fc1aaae187e52a661e21da0a3a693fae1ab7241b
Hugo Leonardo <59965105+hugolps@users.noreply.github.com>
2021-11-28 21:40:27 -0300

    styling

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardsHomeComponent } from './cards-home/cards-home.component';
+import {RouterModule} from "@angular/router";
+import {MatInputModule} from "@angular/material/input";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { CardsHomeComponent } from './cards-home/cards-home.component';
     CardsHomeComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule  ]
 })
 export class HomeModule { }
